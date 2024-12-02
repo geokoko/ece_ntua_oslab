52b2b141d5818b158133d0ef254bec2dbad71f45
geokoko <geokoko3@outlook.com.gr>
2024-12-03 01:18:17 +0200

    Update .gitignore to exclude object, kernel and cmd files

--- lunix-tng/.lunix.o.cmd
+++
@@ -1,3 +0,0 @@
-savedcmd_/home/user/shared/oslab/lunix-tng/lunix.o := ld -m elf_x86_64 -z noexecstack   -r -o /home/user/shared/oslab/lunix-tng/lunix.o @/home/user/shared/oslab/lunix-tng/lunix.mod  ; ./tools/objtool/objtool --hacks=jump_label --hacks=noinstr --hacks=skylake --ibt --orc --retpoline --rethunk --static-call --uaccess --prefix=16  --link  --module /home/user/shared/oslab/lunix-tng/lunix.o
-
-/home/user/shared/oslab/lunix-tng/lunix.o: $(wildcard ./tools/objtool/objtool)
